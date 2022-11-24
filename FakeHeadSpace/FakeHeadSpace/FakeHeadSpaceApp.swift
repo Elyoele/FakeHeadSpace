07fd9b40b8920e1af1d9d2f1731765b52c1059a8
Elyoele <elyoele@gmail.com>
2022-11-24 17:07:53 +0100

    Ciao

---
+++ FakeHeadSpace/FakeHeadSpace/FakeHeadSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeHeadSpaceApp.swift
+//  FakeHeadSpace
+//
+//  Created by Eleonora Coppola on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FakeHeadSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
